2798983aecf6a1131bada0dafd569cb48bbe75dd
Tamira Vojnar <tamiravojnar@gmail.com>
2017-11-27 15:40:32 -0800

    added Song model

--- src/app.js
+++ src/app.js
@@ -8,6 +8,30 @@ import './css/style.css';
 
 console.log('it loaded!');
 
+const songData = [
+    {
+        title: "Drop It Like It's Hot",
+        year: 2004,
+        artist: "Snoop Dog"
+    },
+    {
+      title: 'Song 2',
+      year: 2017,
+      artist: 'Unknown'
+    },
+    {
+      title: 'Song 3',
+      year: 2001,
+      artist: 'Unknown 2'
+    },
+    {
+      title: 'Song 4',
+      year: 2001,
+      artist: 'Unknown 3'
+    }
+  ];
+
+
 $(document).ready( () => {
-  
+
 });
---
+++ src/models/song.js
@@ -0,0 +1,5 @@
+import Backbone from 'backbone';
+
+const Song = Backbone.Model.extend({});
+
+extend default Song; 
